e762e8b1a7cec8c8e2c793b364720009b51ca8dd
adi-278 <me.adityajoshi2226@gmail.com>
2023-04-26 09:23:16 +0530

    Added: calculation salary of employee

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,12 +1,27 @@
-#!/bin/bash  -x
+
+
+
+#!/bin/bash -x
 
 echo "Welcome To Employee Wage Computation Program"
 
-attendence_chk=$((RANDOM%2))
+# Variable for checking the attendence
+attendence_chk=$((RANDOM%2)) #0/1
 
-if [ $attendence_chk -eq 1 ]
+# constant variables
+is_present=1
+full_day_hour=8
+wage_per_hour=20
+
+# initializing the variables
+salary=0
+
+# To check the attendence of the employee
+if [ $attendence_chk -eq $is_present ]
 then
-    echo "Employee is Present"
+    salary=$(($full_day_hour * $wage_per_hour))
+    echo "The Employee is Present for Full day and the Salary is $salary"
 else
-    echo "Employee is Absent"
+    echo "Employee is Absent and the salary is $salary"
 fi
+
