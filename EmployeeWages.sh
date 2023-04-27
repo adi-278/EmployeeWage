6de399f4f5039aa387803e57f5597abe3ab79a60
adi-278 <me.adityajoshi2226@gmail.com>
2023-04-27 08:50:17 +0530

    Added: Welcome to Employee Wage Computing Program

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,4 +1,4 @@
-
+BOB
 
 #!/bin/bash -x
 
@@ -54,3 +54,42 @@ then
 else
     echo "The Employee is Absent and the salary is $salary"
 fi
+
+
+
+
+
+
+#!/bin/bash
+
+echo "Welcome To Employee Wage Computation Program"
+
+# Variable for checking the attendence
+attendence_chk=$((RANDOM%3)) #0/1/2
+
+# constant variables
+is_present_full_time=1
+is_present_part_time=2
+full_day_hour=8
+part_time_hour=4
+wage_per_hour=20
+
+
+# initializing the variables
+salary=0
+daily_hour=0
+
+case $attendence_chk in
+
+$is_present_full_time) daily_hour=8
+                       echo "The Employee is Present for Full day"
+                       ;;
+$is_present_part_time) daily_hour=4
+                       echo "The Employee is Present for Half day"
+                       ;;
+0) echo "The Employee is Absent"
+;;
+esac
+
+salary=$(($daily_hour * $wage_per_hour))
+echo "Salary of an Employee is $salary"
