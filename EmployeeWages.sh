44fcf3003ae6966adb625c6e1f4d7bf954a30479
adi-278 <me.adityajoshi2226@gmail.com>
2023-04-28 08:09:11 +0530

    Added: Calculating wage for part time

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -172,3 +172,56 @@ done
 
 monthly_salary=$(($total_working_hour * $wage_per_hour))
 echo "The Monthly Salary of an Employee is $monthly_salary"
+
+
+
+
+
+
+
+
+#UC6
+# constant variables
+is_present_full_time=1
+is_present_part_time=2
+full_day_hour=8
+part_time_hour=4
+wage_per_hour=20
+total_working_days=20
+total_working_hours=100
+
+
+# initializing the variables
+salary=0
+daily_hour=0
+total_working_hour=0
+day=1
+
+while (( $day <= $total_working_days && $total_working_hour <= $total_working_hours))
+do
+
+    # Variable for checking the attendence
+    attendence_chk=$((RANDOM%3)) #0/1/2
+
+    case $attendence_chk in
+
+    $is_present_full_time) daily_hour=8
+                        echo "The Employee is Present for Full day"
+                        ;;
+    $is_present_part_time) daily_hour=4
+                        echo "The Employee is Present for Half day"
+                        ;;
+    0)daily_hour=0
+      echo "The Employee is Absent"
+    ;;
+    esac
+
+    salary=$(($daily_hour * $wage_per_hour))
+    echo "Salary of an Employee is $salary"
+    total_working_hour=$(($total_working_hour + $daily_hour))
+    ((day++))
+ 
+done
+
+monthly_salary=$(($total_working_hour * $wage_per_hour))
+echo "The Monthly Salary of an Employee is $monthly_salary"
